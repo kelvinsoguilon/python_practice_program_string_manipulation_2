e72e23b82a676cd73c4b3e7947d467633ba2c753
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-27 00:05:41 +0800

    Checks if character is in uppercase

--- check_character_upper_case.py
+++ check_character_upper_case.py
@@ -3,4 +3,7 @@ fullname = "Kelvin Soguilon"
 #set the upper case range
 upper_case = ('A', 'Z')
 #check if char is uppercase
+for char in fullname:
+    if char in upper_case:
+        break
 #print not all character is in uppercase
