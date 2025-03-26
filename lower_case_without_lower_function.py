d594102067262649d38b2fde603c8b3e2c09a196
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-26 23:53:47 +0800

    Return the value to formatted string so it does not return to 0, fixed missing + sign in line 8. Prints output in lowercase without using the lower function

--- lower_case_without_lower_function.py
+++ lower_case_without_lower_function.py
@@ -5,7 +5,9 @@ def lowercase(text):
 #check if the characters are in uppercase
     for char in text:
         if 'A' <= char <= 'Z':
-            formatted_string = chr(ord(char) + 32)
+            formatted_string += chr(ord(char) + 32)
         else:
             formatted_string += char
+    return formatted_string
 #print output
+print(lowercase("Kelvin Soguilon"))
