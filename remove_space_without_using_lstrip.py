303afad240523d8e4e02d240cea0843ca615eebf
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-26 23:15:17 +0800

    Set a fixed string, and added variable to store text after skipping leading spaces

--- remove_space_without_using_lstrip.py
+++ remove_space_without_using_lstrip.py
@@ -1,4 +1,6 @@
 #set a string example
+fullname = "     Kelvin Soguilon"
 #set variable that stores text after ignoring spaces
+space_found = ""
 #check if there are leading spaces and skip it
 #print the result without the leading spaces
