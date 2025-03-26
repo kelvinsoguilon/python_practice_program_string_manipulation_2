e7010757143bc758dd03634b49633a00ce3551c8
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-26 23:21:50 +0800

    Fixed variable names, Checks if there are leading spaces and skips them

--- remove_space_without_using_lstrip.py
+++ remove_space_without_using_lstrip.py
@@ -1,6 +1,11 @@
 #set a string example
 fullname = "     Kelvin Soguilon"
 #set variable that stores text after ignoring spaces
-space_found = ""
+fullname_formatted = ""
 #check if there are leading spaces and skip it
+space_found = True
+for char in fullname:
+    if char != " " or not space_found:
+        fullname_formatted += char
+        space_found = False
 #print the result without the leading spaces
