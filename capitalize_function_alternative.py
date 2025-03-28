7eb468d765a90eb006d46f168639e029a23a0d22
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-28 16:43:23 +0800

    Define capitalized text, set variable to store the the string input, and split the string into words

--- capitalize_function_alternative.py
+++ capitalize_function_alternative.py
@@ -1,6 +1,9 @@
 #define capitalized text
+def capitalize(text):
 #set variable to store string
+    formatted_string = ""
 #split string into word
+    words = formatted_string.split()
 #iterate through every first letter of the word
 #capitalize the first letter and add the rest unchanged
 #print result
