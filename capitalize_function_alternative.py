a2685db2c1ba66a871bf976104d901ce3e86a5eb
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-28 16:50:27 +0800

    Fixed variable error, Prints the correct output where it capitalizes every first letter of the word, and removes the extra space at the end

--- capitalize_function_alternative.py
+++ capitalize_function_alternative.py
@@ -3,9 +3,11 @@ def capitalize(text):
 #set variable to store string
     formatted_string = ""
 #split string into word
-    words = formatted_string.split()
+    words = text.split()
 #iterate through every first letter of the word
     for word in words:
 #capitalize the first letter and add the rest unchanged
         formatted_string += word[0].upper() + word[1:] + " "
 #print result
+    return formatted_string.strip()
+print(capitalize("kelvin soguilon"))
