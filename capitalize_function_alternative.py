57137a16a7af4a6393669c4b0bdab83f6c6a5b79
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-28 16:41:42 +0800

    Added Pseudocode

---
+++ capitalize_function_alternative.py
@@ -0,0 +1,6 @@
+#define capitalized text
+#set variable to store string
+#split string into word
+#iterate through every first letter of the word
+#capitalize the first letter and add the rest unchanged
+#print result
