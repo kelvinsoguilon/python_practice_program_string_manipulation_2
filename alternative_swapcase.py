ef7470999952ce99d2df05d890c0c99f7100dda6
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-28 16:21:29 +0800

    Added variable to store string input

--- alternative_swapcase.py
+++ alternative_swapcase.py
@@ -1,5 +1,7 @@
 #define swapcase
 def swapcase(text):
+#set variable to store text
+    formatted_string = ""
 #iterate through every char
 #check if uppercase or lowercase and reverse it
 #print output
