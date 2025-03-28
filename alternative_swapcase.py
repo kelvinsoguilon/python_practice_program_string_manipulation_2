ff578370998cdab40dcee2dcdbf522e29dca5cbb
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-28 16:23:41 +0800

    Iterate through every character and checks if in uppercase or lowercase, then reverses it

--- alternative_swapcase.py
+++ alternative_swapcase.py
@@ -3,5 +3,10 @@ def swapcase(text):
 #set variable to store text
     formatted_string = ""
 #iterate through every char
+    for char in text:
 #check if uppercase or lowercase and reverse it
+        if 'A' <= char <= 'Z':
+            formatted_string += chr(ord(char) + 32)
+        else:
+            formatted_string += chr(ord(char) - 32)
 #print output
