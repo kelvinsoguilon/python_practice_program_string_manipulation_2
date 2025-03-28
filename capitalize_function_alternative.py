6cdcc8a6871b08f62f2db656ed123a74ef76eb4f
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-28 16:48:53 +0800

    Iterate through every first letter of the words and capitalized it

--- capitalize_function_alternative.py
+++ capitalize_function_alternative.py
@@ -5,5 +5,7 @@ def capitalize(text):
 #split string into word
     words = formatted_string.split()
 #iterate through every first letter of the word
+    for word in words:
 #capitalize the first letter and add the rest unchanged
+        formatted_string += word[0].upper() + word[1:] + " "
 #print result
