a9c323c0f1e29b272301fab04086f66d52156583
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-04-02 00:05:41 +0800

    Set a string sample, and define the range of lowercase letters

--- batch_7/islower_function_alternative.py
+++ batch_7/islower_function_alternative.py
@@ -1,4 +1,6 @@
 #set string example
+text = "Kelvin Soguilon"
 #define the range of lowercase letters
+lowercase = ("a", "z")
 #identify if character is on lowercase
 #print not all character are in lowercase
