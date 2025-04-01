c775d157e0da2f4f5b06050cf1900b09dc6ae3dd
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-04-01 23:09:19 +0800

    Divide the padding needed in left and right spaces of the text, return text in center format

--- center_alternative.py
+++ center_alternative.py
@@ -5,5 +5,8 @@ def alternative_center(text, width, fillchar = " ")
 #get the padding needed
     padding_needed = max(0, width - text_length)
 #divide padding in left and right
+    left_pad = padding_needed // 2
+    right_pad = padding_needed - left_pad
 #return characters in center string format
+    return (fillchar * left_pad) + text + (fillchar * right_pad)
 #print result
