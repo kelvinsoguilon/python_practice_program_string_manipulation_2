ec826e4ad3c2928c5f447bc14cf1890c1608cce9
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-04-01 23:59:36 +0800

    Set a string example, Iterates through every char and checks if on lowercase, makes it uppercase, otherwise keep the same text

--- batch_7/upper_function_alternative.py
+++ batch_7/upper_function_alternative.py
@@ -1,4 +1,11 @@
 #set string example
+text = "Kelvin Soguilon"
+formatted_text = ""
 #checks if char is on lowercase
+for char in text:
+    if 'a' <= char <= 'z':
+        formatted_text += chr(ord(char) - 32)
 #otherwise keep the same text
+    else:
+        formatted_text += char
 #print result
