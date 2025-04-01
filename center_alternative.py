86584a1c6e87cae87003b79360e89934c910d4ff
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-04-01 23:05:04 +0800

    Get the length of the string as well as the padding needed to fill the spaces

--- center_alternative.py
+++ center_alternative.py
@@ -1,6 +1,9 @@
 #define center function
+def alternative_center(text, width, fillchar = " ")
 #get string length
+    text_length = len(text)
 #get the padding needed
+    padding_needed = max(0, width - text_length)
 #divide padding in left and right
 #return characters in center string format
 #print result
