80616fd73d021a77dfee59a3b2585aadb737a922
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-04-02 00:06:57 +0800

    Iterates through every character and checks if on lowercase

--- batch_7/islower_function_alternative.py
+++ batch_7/islower_function_alternative.py
@@ -3,4 +3,7 @@ text = "Kelvin Soguilon"
 #define the range of lowercase letters
 lowercase = ("a", "z")
 #identify if character is on lowercase
+for char in text:
+    if char in lowercase:
+        break
 #print not all character are in lowercase
