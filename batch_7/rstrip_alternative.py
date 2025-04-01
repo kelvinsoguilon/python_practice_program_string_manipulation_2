9f8dd0dc59a6de0db80e6e2fea2b519f0c59ba02
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-04-01 23:33:00 +0800

    Set a string example, Added a variable to store the text in correct format

--- batch_7/rstrip_alternative.py
+++ batch_7/rstrip_alternative.py
@@ -1,5 +1,8 @@
 #set string example
+text = "   Kelvin Soguilon      "
 #set variable to store the text in correct format
+formatted_text = ""
 #identify space characters
+space_found = True
 #iterate through every character and check if there is space
 #print result
