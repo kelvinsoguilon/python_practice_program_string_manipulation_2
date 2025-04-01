03162fac8a93dd88f399138119e6a5860e300013
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-04-01 21:52:17 +0800

    Set variable to store the texts

--- title_alternative.py
+++ title_alternative.py
@@ -1,4 +1,6 @@
 #define title function
+def title(text):
 #set a variable to store the texts
+    format_title = ""
 #capitalize every first letter of word while keeping rest on lowercase
 #print output on title casing
