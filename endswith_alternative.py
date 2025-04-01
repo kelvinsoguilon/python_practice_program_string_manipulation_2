e1577d6168dcaf741c3e13eca625babbfc21436a
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-04-01 22:21:11 +0800

    Get the length of the suffix and extract the end part of the string same as suffix

--- endswith_alternative.py
+++ endswith_alternative.py
@@ -1,5 +1,8 @@
 #define endswith function
+def endswith(text, suffix):
 #get length of suffix
+    suffix_length = len(suffix)
 #extract end part of string with same length as suffix
+    end_part = text[- suffix_length:]
 #compare extracted part and suffix
 #print result
