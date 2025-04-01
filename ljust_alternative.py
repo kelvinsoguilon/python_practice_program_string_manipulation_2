1d9f88d92b16648fbb57abbd84936c3ab9355a18
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-04-01 22:49:04 +0800

    Get string length and the amount of characters you need to fill

--- ljust_alternative.py
+++ ljust_alternative.py
@@ -1,5 +1,8 @@
 #define ljust function
+def ljust(text, width, char_fill = " "):
 #get length of string
+    text_length = len(text)
 #get the padding characters needed
+    padding_char = max(0, width - text_length)
 #fill characters to the end of the string
 #print result
