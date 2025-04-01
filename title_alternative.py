f050400e7c06e507c21dafbd45d892854a91a3f9
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-04-01 21:54:26 +0800

    Split string into words. capitalize every first letter while keeping the rest on lowercase

--- title_alternative.py
+++ title_alternative.py
@@ -3,4 +3,7 @@ def title(text):
 #set a variable to store the texts
     format_title = ""
 #capitalize every first letter of word while keeping rest on lowercase
+    words = text.split()
+    for word in words:
+        format_title += word[0].upper() + word[1:].lower() + " "
 #print output on title casing
