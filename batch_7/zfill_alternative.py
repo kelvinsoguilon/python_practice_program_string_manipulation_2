4e60c585ac4075c4086ed72bda5093a62a5b656a
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-04-02 10:50:04 +0800

    Fixed logic mistake on string input. Prints correct output

--- batch_7/zfill_alternative.py
+++ batch_7/zfill_alternative.py
@@ -1,6 +1,6 @@
 #ask for number from 0-1000
-number = int(input("Enter a number from 0-1000"))
-if number < 1000:
+number = (input("Enter a number from 0-1000: "))
+if int(number) > 1000:
     print("Not a valid number.")
     exit()
 #count how many zeros u need to fill
@@ -8,3 +8,4 @@ zero_fill = 6 - len(number)
 #combine the number of zeros and the number input
 result = "0" * zero_fill + number
 #print result
+print(result)
