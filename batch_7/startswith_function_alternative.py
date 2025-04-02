1f5295620145f6fd4ae3b9e39e87b129ed3c8107
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-04-02 21:39:34 +0800

    Return string as first word is equals to prefix or the start of the string

--- batch_7/startswith_function_alternative.py
+++ batch_7/startswith_function_alternative.py
@@ -3,4 +3,5 @@ def startswith_alternative(text, prefix):
 #return the function and split the string into words. only take the first word
     first_word = text.split()[0] if text.split() else ""
 #compare the first word to the splitted part
+    return first_word == prefix
 #print result
