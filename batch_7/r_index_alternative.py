edc00a3060358044f31044df7abe3fa2cb42f466
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-04-02 22:32:45 +0800

    Get text length and substring length

--- batch_7/r_index_alternative.py
+++ batch_7/r_index_alternative.py
@@ -1,5 +1,8 @@
 #define rindex function
+def rindex_alternative(text, substring):
 #get text length and substring length
+    text_length = len(text)
+    substring_length = len(substring)
 #iterate through text beginning in rightmost part and check first occurence of substring
 #return value to i
 #print output 
