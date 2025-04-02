fe4e51142ec90996b17da9c4f2689a1732631804
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-04-02 10:45:29 +0800

    Count how many zeros are needed to add in the beginning

--- batch_7/zfill_alternative.py
+++ batch_7/zfill_alternative.py
@@ -2,7 +2,8 @@
 number = int(input("Enter a number from 0-1000"))
 if number < 1000:
     print("Not a valid number.")
-    break
+    exit()
 #count how many zeros u need to fill
+zero_fill = 6 - len(number)
 #combine the number of zeros and the number input
 #print result
