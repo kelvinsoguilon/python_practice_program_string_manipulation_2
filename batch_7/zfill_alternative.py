65adee9f8802745e40b9ff01b2e262e0953cf737
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-04-02 10:42:39 +0800

    Ask for number from 0 to 1000. Handle cases where number input is greater than 1000

--- batch_7/zfill_alternative.py
+++ batch_7/zfill_alternative.py
@@ -1,4 +1,8 @@
 #ask for number from 0-1000
+number = int(input("Enter a number from 0-1000"))
+if number < 1000:
+    print("Not a valid number.")
+    break
 #count how many zeros u need to fill
 #combine the number of zeros and the number input
 #print result
