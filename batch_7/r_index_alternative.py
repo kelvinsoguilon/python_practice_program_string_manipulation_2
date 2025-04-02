57d33f9648d7b4657db5c6cc0b18fd6fec07e36c
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-04-02 22:31:43 +0800

    Added steps to code the program

---
+++ batch_7/r_index_alternative.py
@@ -0,0 +1,5 @@
+#define rindex function
+#get text length and substring length
+#iterate through text beginning in rightmost part and check first occurence of substring
+#return value to i
+#print output 
