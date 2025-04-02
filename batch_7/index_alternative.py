c03ee0b7af4652503e7a149c70364c79c096609a
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-04-02 22:24:16 +0800

    Iterate through text and finds first occurence of the substring. Return value to i.

--- batch_7/index_alternative.py
+++ batch_7/index_alternative.py
@@ -4,5 +4,8 @@ def index_alternaative(text, substring):
     text_length = len(text)
     substring_length = len(substring)
 #iterate through text and find first occurence of substring
+    for i in range(text_length - substring_length + 1):
+        if text[i:i + substring_length] == substring:
 #return i value
+            return i
 #print result
