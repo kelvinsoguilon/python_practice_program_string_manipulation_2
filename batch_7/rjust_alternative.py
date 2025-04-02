3f118959b4ad56fe64c53adb57be29dd2988e20c
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-04-02 21:50:59 +0800

    Define rjust function, get string length, and padding characters needed

--- batch_7/rjust_alternative.py
+++ batch_7/rjust_alternative.py
@@ -1,5 +1,8 @@
 #define rjust
+def rjust(text, width, fillchar = " "):
 #get length of string
+    text_length = len(text)
 #get the count of padding characters needed
+    padding_needed = max(0, width - text_length)
 #return string in rjust format
 #print output
