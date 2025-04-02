248ab5f20691ed3c15bb7735448e74dacb3ec48f
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-04-02 22:02:26 +0800

    Set count to 0, get length of both text and substring

--- batch_7/count_function_alternative.py
+++ batch_7/count_function_alternative.py
@@ -1,6 +1,10 @@
 #define count function
+def count_alternative(text, substring):
 #set count variable to 0 to store the number of appearance
+    count = 0
 #get length of text and substring
+    text_length = len(text)
+    substring_length = len(substring)
 #iterate through text and check for substring occurences
 #return count
 #print result
