ad652cd4b67dff27038824296399615b52aa0691
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-04-02 21:59:21 +0800

    Added Algorithm

---
+++ batch_7/count_function_alternative.py
@@ -0,0 +1,6 @@
+#define count function
+#set count variable to 0 to store the number of appearance
+#get length of text and substring
+#iterate through text and check for substring occurences
+#return count
+#print result
