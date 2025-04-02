e591a657590dce95181db85d5e762f0f2426c770
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-04-02 22:22:11 +0800

    Get length of the text and the substring

--- batch_7/index_alternative.py
+++ batch_7/index_alternative.py
@@ -1,5 +1,8 @@
 #define index function
+def index_alternaative(text, substring):
 #get text length and substring length
+    text_length = len(text)
+    substring_length = len(substring)
 #iterate through text and find first occurence of substring
 #return i value
 #print result
