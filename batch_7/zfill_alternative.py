f8a79cdf953e7fc4b29570c87e22fde5f51079e2
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-04-02 10:47:32 +0800

    Combine the zeros and the number input

--- batch_7/zfill_alternative.py
+++ batch_7/zfill_alternative.py
@@ -6,4 +6,5 @@ if number < 1000:
 #count how many zeros u need to fill
 zero_fill = 6 - len(number)
 #combine the number of zeros and the number input
+result = "0" * zero_fill + number
 #print result
